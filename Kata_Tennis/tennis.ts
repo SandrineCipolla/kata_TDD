eda6b425ffff03e52f6a2eb46f5a3873a4475878
Sandrine Cipolla <sandrine.cipolla@gmail.com>
2023-11-05 16:32:38 +0100

    refactoring : function playerScore

--- Kata_Tennis/tennis.ts
+++ Kata_Tennis/tennis.ts
@@ -1,18 +1,27 @@
-const Love : string = "Love"
+const Love: string = "Love";
 
-const computeScore = (playerOneScore : number, playerTwoScore : number) : string => {
-    if (playerOneScore === 4) return "playerOne wins";
-    if (playerOneScore === 3) return formatScore("40",Love);
-    if (playerOneScore === 2) return formatScore("30",Love);
-    if (playerOneScore === 1) return formatScore("15",Love);
-    
-    return formatScore(Love,Love);
-    
+const computeScore = (
+  playerOneScore: number,
+  playerTwoScore: number
+): string => {
+  if (playerOneScore === 4) return "playerOne wins";
+  if (playerOneScore === 3)
+    return formatScore(playerScore(playerOneScore), Love);
+  if (playerOneScore === 2)
+    return formatScore(playerScore(playerOneScore), Love);
+  if (playerOneScore === 1)
+    return formatScore(playerScore(playerOneScore), Love);
+
+  return formatScore(playerScore(playerOneScore), Love);
+};
+function playerScore(score: number): string {
+  if (score === 3) return "40";
+  if (score === 2) return "30";
+  if (score === 1) return "15";
+  return Love;
 }
 function formatScore(playerOneScore: string, playerTwoScore: string): string {
-    return playerOneScore + " - " + playerTwoScore;
+  return playerOneScore + " - " + playerTwoScore;
 }
 
-export default computeScore
-
-
+export default computeScore;
