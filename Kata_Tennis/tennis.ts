e8f724dd54ecfcd7355591782639105144f95e0b
Sandrine Cipolla <sandrine.cipolla@gmail.com>
2023-11-05 16:21:09 +0100

    playerOne scores alone until winning

--- Kata_Tennis/tennis.ts
+++ Kata_Tennis/tennis.ts
@@ -1,5 +1,18 @@
+const Love : string = "Love"
+
 const computeScore = (playerOneScore : number, playerTwoScore : number) : string => {
-    return 'Love - Love';
+    if (playerOneScore === 4) return "playerOne wins";
+    if (playerOneScore === 3) return formatScore("40",Love);
+    if (playerOneScore === 2) return formatScore("30",Love);
+    if (playerOneScore === 1) return formatScore("15",Love);
+    
+    return formatScore(Love,Love);
+    
+}
+function formatScore(playerOneScore: string, playerTwoScore: string): string {
+    return playerOneScore + " - " + playerTwoScore;
 }
 
 export default computeScore
+
+
--- Kata_Tennis/tests/tennis.test.ts
+++ Kata_Tennis/tests/tennis.test.ts
@@ -1,13 +1,45 @@
-import {describe ,test,expect, it} from '@jest/globals'
-import computeScore from '../tennis';
+import { describe, test, expect, it } from "@jest/globals";
+import computeScore from "../tennis";
 
-describe ("Tennis Kata", () => {
-    it("le jeu démarre, le score est de Love - Love ", () => {
-        let playerOneScore = 0;
-        let playerTwoScore = 0;
-        
-        let score = computeScore (playerOneScore, playerTwoScore);
-  
-        expect(score).toBe('Love - Love');
-      });
-})
+describe("Tennis Kata", () => {
+  it("le jeu démarre, le score est de Love - Love ", () => {
+    let playerOneScore = 0;
+    let playerTwoScore = 0;
+
+    let score = computeScore(playerOneScore, playerTwoScore);
+
+    expect(score).toBe("Love - Love");
+  });
+  it("le joueur 1 marque, le score est de 15 - Love ", () => {
+    let playerOneScore = 1;
+    let playerTwoScore = 0;
+
+    let score = computeScore(playerOneScore, playerTwoScore);
+
+    expect(score).toBe("15 - Love");
+  });
+  it("le joueur 1 a marqué 2 fois, le score est de 30 - Love ", () => {
+    let playerOneScore = 2;
+    let playerTwoScore = 0;
+
+    let score = computeScore(playerOneScore, playerTwoScore);
+
+    expect(score).toBe("30 - Love");
+  });
+  it("le joueur 1 a marqué 3 fois, le score est de 40 - Love ", () => {
+    let playerOneScore = 3;
+    let playerTwoScore = 0;
+
+    let score = computeScore(playerOneScore, playerTwoScore);
+
+    expect(score).toBe("40 - Love");
+  });
+  it("le joueur 1 a marqué 4 fois, le joueur 1 gagne ", () => {
+    let playerOneScore = 4;
+    let playerTwoScore = 0;
+
+    let score = computeScore(playerOneScore, playerTwoScore);
+
+    expect(score).toBe("playerOne wins");
+  });
+});
